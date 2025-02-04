5745d1e128415ad8a0d90df1ea02a10e12bebd8f
penso996 <penso.andrea@outlook.it>
2025-02-04 18:31:58 +0100

    fixed image source on post array in index.js

--- index.js
+++ index.js
@@ -10,31 +10,31 @@ const posts = [
     {
         title: "Ciambellone",
         content: "Come preparare un ottimo ciambellone",
-        image: "http://localhost:3000/ciambellone.jpeg",
+        image: "http://localhost:3000/images/ciambellone.jpeg",
         tags: ["dolci", "cucina"]
     },
     {
         title: "Cracker Barbabietola",
         content: "Sfiziosi crackers",
-        image: "http://localhost:3000/cracker_barbabietola.jpeg",
+        image: "http://localhost:3000/images/cracker_barbabietola.jpeg",
         tags: ["cucina", "snack"]
     },
     {
         title: "Pane Fritto Dolce",
         content: "Una pietanza molto leggera",
-        image: "http://localhost:3000/pane_fritto_dolce.jpeg",
+        image: "http://localhost:3000/images/pane_fritto_dolce.jpeg",
         tags: ["cucina", "fritto"]
     },
     {
         title: "Pasta con Barbabietola",
         content: "Come preparare questo piatto particolare",
-        image: "http://localhost:3000/pasta_barbabietola.jpeg",
+        image: "http://localhost:3000/images/pasta_barbabietola.jpeg",
         tags: ["pasta", "barbabietola"]
     },
     {
         title: "Torta Paesana",
         content: "Preparare questo piatto tipico di paese",
-        image: "http://localhost:3000/torta_paesana.jpeg",
+        image: "http://localhost:3000/images/torta_paesana.jpeg",
         tags: ["torta", "pasticceria"]
     }
 ];

d56bb08ebfe6d5a4508daacb1a115c9ec19df62f
penso996 <penso.andrea@outlook.it>
2025-02-04 18:48:30 +0100

    fixed array inside app.get/bacheca

--- index.js
+++ index.js
@@ -3,41 +3,7 @@ const app = express();
 const port = 3000;
 
 // Cartella per file statici
-app.use(express.static("public/images"))
-
-// Array di post
-const posts = [
-    {
-        title: "Ciambellone",
-        content: "Come preparare un ottimo ciambellone",
-        image: "http://localhost:3000/images/ciambellone.jpeg",
-        tags: ["dolci", "cucina"]
-    },
-    {
-        title: "Cracker Barbabietola",
-        content: "Sfiziosi crackers",
-        image: "http://localhost:3000/images/cracker_barbabietola.jpeg",
-        tags: ["cucina", "snack"]
-    },
-    {
-        title: "Pane Fritto Dolce",
-        content: "Una pietanza molto leggera",
-        image: "http://localhost:3000/images/pane_fritto_dolce.jpeg",
-        tags: ["cucina", "fritto"]
-    },
-    {
-        title: "Pasta con Barbabietola",
-        content: "Come preparare questo piatto particolare",
-        image: "http://localhost:3000/images/pasta_barbabietola.jpeg",
-        tags: ["pasta", "barbabietola"]
-    },
-    {
-        title: "Torta Paesana",
-        content: "Preparare questo piatto tipico di paese",
-        image: "http://localhost:3000/images/torta_paesana.jpeg",
-        tags: ["torta", "pasticceria"]
-    }
-];
+app.use(express.static("public"))
 
 // Rotta home
 app.get("/", (req, res) => {
@@ -46,6 +12,41 @@ app.get("/", (req, res) => {
 
 // Rotta bacheca che restituisca il json dei post
 app.get("/bacheca", (req, res) => {
+
+    // Array di post
+    const posts = [
+        {
+            title: "Ciambellone",
+            content: "Come preparare un ottimo ciambellone",
+            image: "/images/ciambellone.jpeg",
+            tags: ["dolci", "cucina"]
+        },
+        {
+            title: "Cracker Barbabietola",
+            content: "Sfiziosi crackers",
+            image: "/images/cracker_barbabietola.jpeg",
+            tags: ["cucina", "snack"]
+        },
+        {
+            title: "Pane Fritto Dolce",
+            content: "Una pietanza molto leggera",
+            image: "/images/pane_fritto_dolce.jpeg",
+            tags: ["cucina", "fritto"]
+        },
+        {
+            title: "Pasta con Barbabietola",
+            content: "Come preparare questo piatto particolare",
+            image: "/images/pasta_barbabietola.jpeg",
+            tags: ["pasta", "barbabietola"]
+        },
+        {
+            title: "Torta Paesana",
+            content: "Preparare questo piatto tipico di paese",
+            image: "/images/torta_paesana.jpeg",
+            tags: ["torta", "pasticceria"]
+        }
+    ];
+
     res.json(posts);
 })
 
